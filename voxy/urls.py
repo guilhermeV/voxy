f54ffaddcde487240090e6f8f2a6240a6768027d
guilhermeV <guilherme.vinicius.a.p@gmail.com>
2018-02-12 15:33:11 -0200

    Fix

--- voxy/urls.py
+++ voxy/urls.py
@@ -25,3 +25,7 @@ urlpatterns = [
     url(r'^count/', views.get_text, name='count'),
 ]
 
+urlpatterns += urlpatterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+    {'document_root': os.path.join(os.path.dirname(__file__), 'static')} ),
+    )
