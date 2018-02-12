449b7913e59fe49b5d29072a078c9b7fca08e71a
guilhermeV <guilherme.vinicius.a.p@gmail.com>
2018-02-12 16:18:42 -0200

    Fixwed

--- voxy/views.py
+++ voxy/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from .forms import WordCounterForm
 from collections import Counter
@@ -9,23 +10,23 @@ def index(request):
     return HttpResponse("Hello moto")
     
 def get_text(request):
+    result = '?'
     if request.method == 'POST':
         form = WordCounterForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            counter(form.cleaned_data['text_block'])
-            
+            result = counter(form.cleaned_data['text_block'])
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            return render(request, 'name.html', {'form': form, 'result': result})        
 
     else:
         form = WordCounterForm()
-
-    return render(request, 'name.html', {'form': form})
+        
+    return render(request, 'name.html', {'form': form, 'result': result})
     
 def counter(text_block):
+    cnt = Counter(text_block)
     words = re.findall(r'\w+', text_block.lower())
-    
-    
+    return {'quantity': len(words), 'distinct': len(Counter(words))}
